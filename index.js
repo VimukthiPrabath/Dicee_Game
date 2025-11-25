537c2ac8a69d5ab51da8f0f6795a967ac71e043d
VimukthiPrabhath <vimukthivpk123@gmail.com>
2025-11-25 16:05:39 +0530

    Dicee Game Final

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomDiceImage2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins! Congrats!";
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = " 🚩 Player 2 Wins! Congrats!";
 } else {
   document.querySelector("h1").innerHTML = "It's a Draw!";
 }
